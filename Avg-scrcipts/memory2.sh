53da8f177c1c01fbbd96f7f86fed86d27fa3abe0
Mays-Waddah <mays.w.zk@gmail.com>
2022-09-13 12:21:03 +0300

    Update memory2.sh

--- Avg-scrcipts/memory2.sh
+++ Avg-scrcipts/memory2.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+count=0;
+total=0; 
+
+for i in $( awk '{ print $2; }' memory-avg.txt )
+   do 
+     total=$(echo $total+$i | bc )
+     ((count++))
+   done
+echo "Average of memory is : $total / $count" | bc "
