ec44b7ce88f700995bbe8463a3560c42d79a9f8c
Mays-Waddah <mays.w.zk@gmail.com>
2022-09-13 12:20:29 +0300

    Update disk2.sh

--- Avg-scrcipts/disk2.sh
+++ Avg-scrcipts/disk2.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+count=0;
+total=0; 
+
+for i in $( awk '{ print $2; }' disk-avg.txt )
+   do 
+     total=$(echo $total+$i | bc )
+     ((count++))
+   done
+echo "Average of disk is : $total / $count" | bc "
