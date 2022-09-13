d05eaff93d20dceafc950c25314aacde6fd39c34
Mays-Waddah <mays.w.zk@gmail.com>
2022-09-13 03:11:12 +0300

    Update htmls.sh

--- htmls.sh
+++ htmls.sh
@@ -6,17 +6,25 @@
 
 echo "CPU Usage : "  >> cpu.html 
 echo " "    >> cpu.html 
-vmstat | awk '{print $6} '  >> cpu.html 
+top | grep %Cpu  >> cpu.html 
 echo " " >> cpu.html > /dev/null
+vmstat | awk '{print $6} '  >>cpu-avg.sh
 
 
+#used memory
 echo "Memory Usage : "  >> memory.html
 echo " "  >> memory.html 
-free | awk '{print $2} ' >> memory.html 
+free |grep Mem | awk '{print $2,$3,$6} ' >> memory.html 
+free | grep Mem
+awk '{ print $1; }' >>memory-avg.sh
 
 echo "Disk Usage: "  >> disk.html 
 echo " " >> disk.html  
  df >> disk.html 
+ df | awk '{print $4} ' >> disk-avg.sh  
+ #the available space avg
+ 
+
 
 
 
